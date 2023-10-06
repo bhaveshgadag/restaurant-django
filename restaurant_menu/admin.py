487e0072f1686f8b70501e263fd328cfb50b5b3b
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-10-06 17:59:25 +0530

    changed status to int field::Admin interface for items

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('meal', 'description')
+    list_filter = ('status',)
+
+
+admin.site.register(Item, MenuItemAdmin)
