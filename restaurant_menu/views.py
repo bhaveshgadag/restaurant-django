209ec194282f86bf03ce672b8673d8ad4cfac42e
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-10-06 21:17:49 +0530

    get db data:context::display meal type:description

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -8,7 +8,8 @@ class MenuList(generic.ListView):
     template_name = 'index.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = {'meals': MEAL_TYPE}
+        context = super().get_context_data()
+        context['meals'] = MEAL_TYPE
         return context
 
 
